58315ee8bcdc7362b193217e7322513900656ebd
Maksim <lvutinwork@gmail.com>
2023-09-11 22:12:56 +0300

    Stage 1: Создан начальный каркас приложения

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <main className='container content'>
+            Shop
+        </main>
+    );
+};
+
+export default Shop;
