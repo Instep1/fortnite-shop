74426b622e25217b153517f023564e8480f64a89
Maksim <lvutinwork@gmail.com>
2023-09-13 12:36:05 +0300

    Stage 6: В проект добавлен gh-page, а также ссылки на репозиторий

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
             <div className="nav-wrapper">
             <a href="#!" className="brand-logo">React Fortnite Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#!">Repo</a></li>
+                <li><a href="https://github.com/Instep1/fortnite-shop" target="_blank" rel="noreferrer">Repo</a></li>
             </ul>
             </div>
         </nav>
